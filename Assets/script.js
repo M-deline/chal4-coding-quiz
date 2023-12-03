d261ddbf452f40cee25adc479eafcb4e8c41d6c5
Madeline K <madkovanda@gmail.com>
2023-12-02 17:32:50 -0700

    edit js

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,118 @@
 
+let timeElement = document.getElementById("timer");
+let scoreElement = document.getElementById("score");
+
+const questionsElement = document.getElementById("questions");
+let questionElement = document.getElementById("question");
+
+const correctIncorrectElement = document.getElementById("correct-incorrect");
+const finalEL = document.getElementById("final");
+let initials = document.getElementsByClassName("initials")
+
+const highScore = document.getElementById("highscores");
+let scoreEl = document.getElementById("highscore-list");
+let scoreList = [];
+
+let secondsLeft = 75;
+//buttons 
+const startButton = document.querySelector("#start-button");
+// const answerButton = document.querySelector(".answer");
+const firansButton = document.querySelector("#answer-1");
+const secansButton = document.querySelector("#answer-2");
+const thransButton = document.querySelector("#answer-3");
+const fouransButton = document.querySelector("#answer-4");
+const submitButton = document.querySelector("#score-button");
+const returnButton = document.querySelector("#return");
+const clearButton = document.querySelector("#clear");
+
+
+const gameArray = [
+    {
+        question: "Commonly used data types do NOT include",
+        options:
+            ["strings", "boolean", "alerts", "numbers"],
+        correctAnswer: 2
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugging is...",
+        options:
+            ["JavaScript", "terminal/bash", "for loop", "console.log"],
+        correctAnswer: 1
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within ....",
+        options:
+            ["quotes", "parentheses", "curly brackets", "square brackets"],
+        correctAnswer: 1
+    },
+    {
+        question: "String values must be enclosed within ... when being assigned to variables",
+        options:
+            ["commas", "quotes", "curly-brackets", "parentheses"],
+        correctAnswer: 1
+    },
+    {
+        question: "arrays in javascript can be used to store ...",
+        options:
+            ["numbers and strings", "booleans", "other arrays", "all the above"],
+        correctAnswer: 3
+    },
+
+];
+//timer function 
+function setTime() {
+    let timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeElement.textContent = `Time: ${secondsLeft}s`;
+        if (secondsLeft === 0 || questionCount === gameArray.length) {
+            clearInterval(timerInterval);
+            questionsElement.style.display = "none";
+            if (finalEL) {
+                finalEL.style.display = "block";
+            }
+            scoreElement.textContent = secondsLeft;
+        }
+    }, 1000);
+}
+
+function startGame() {
+    questionsElement.style.display = "block";
+    questionCount = 0;
+
+    setTime();
+    nextQuestion(questionCount);
+}
+
+function nextQuestion(id) {
+    if (id < gameArray.length) {
+        questionElement.textContent = gameArray[id].question;
+        firansButton.textContent = gameArray[id].options[0];
+        secansButton.textContent = gameArray[id].options[1];
+        thransButton.textContent = gameArray[id].options[2];
+        fouransButton.textContent = gameArray[id].options[3];
+    }
+}
+
+//check 
+function checkAnswer(event) {
+    event.preventDefault();
+
+    correctIncorrectElement.style.display = "block";
+    let p = document.createElement("p");
+    correctIncorrectElement.appendChild(p);
+
+    setTimeout(function () {
+        p.style.display = "none";
+    }, 1000);
+
+    if (questionCount < gameArray.length) {
+        if (gameArray[questionCount].correctAnswer === event.target.value) {
+            p.textContent = "Correct!";
+        } else {
+            secondsLeft = secondsLeft - 10;
+            p.textContent = "Incorrect!";
+        }
+        questionCount++;
+        nextQuestion(questionCount);
+    }
+}
